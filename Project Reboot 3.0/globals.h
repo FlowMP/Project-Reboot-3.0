088642c13054aa51955b230722706378d46d8f78
deemvfx <71044752+deemvfx@users.noreply.github.com>
2023-06-03 19:44:23 -0700

    Update globals.h

--- Project Reboot 3.0/globals.h
+++ Project Reboot 3.0/globals.h
@@ -9,10 +9,10 @@ namespace Globals
 	extern inline bool bCreative = false;
 	extern inline bool bGoingToPlayEvent = false;
 	extern inline bool bEnableAGIDs = true;
-	extern inline bool bNoMCP = true;
-	extern inline bool bLogProcessEvent = false;
+	extern inline bool bNoMCP = false;
+	extern inline bool bLogProcessEvent = false; 
 	// extern inline bool bLateGame = false;
-	extern inline std::atomic<bool> bLateGame(false);
+	extern inline std::atomic<bool> bLateGame(true);
 
 	extern inline bool bInfiniteMaterials = false;
 	extern inline bool bInfiniteAmmo = false;
